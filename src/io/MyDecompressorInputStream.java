90acbbbf396a1760e0e35e102689c5f8539b94eb
lihis <lihis@Lihi>
2016-09-03 13:33:35 +0300

    add io package

---
+++ src/io/MyDecompressorInputStream.java
@@ -0,0 +1,19 @@
+package io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyDecompressorInputStream extends InputStream{
+
+	InputStream in;
+
+	public MyDecompressorInputStream(InputStream in) {
+		this.in=in;
+	}
+	@Override
+	public int read() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
