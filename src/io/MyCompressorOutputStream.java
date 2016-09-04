b2d2f4aaedfe4dc153398d75e8da2b23535ea096
Lior <Lior@Lior-PC>
2016-09-04 19:39:02 +0300

    upload

--- src/io/MyCompressorOutputStream.java
+++ src/io/MyCompressorOutputStream.java
@@ -3,13 +3,20 @@ package io;
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class MyCompressorOutputStream extends java.io.OutputStream{
+public class MyCompressorOutputStream extends OutputStream{
 
-	OutputStream out;
 	
-	public MyCompressorOutputStream(OutputStream out) {
-		this.out=out;
+	private OutputStream out;
+	
+	public MyCompressorOutputStream() {
+		//out=new Out
 	}
+	
+	/**
+	 * Overrides the write class and writes bytes array to file
+	 * @param1 b 
+	 * 
+	 */
 	@Override
 	public void write(int b) throws IOException {
 		// TODO Auto-generated method stub
